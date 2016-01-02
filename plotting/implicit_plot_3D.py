e6e63cd8149fff37b8cf903a2cdf4b870ce5900d
maayan <maayamoshe@gmail.com>
2016-01-02 11:31:29 +0200

    doc test for plotting

--- plotting/implicit_plot_3D.py
+++ plotting/implicit_plot_3D.py
@@ -13,32 +13,38 @@ def surface_implicit(x, y, z):
     
     return x**2 + y**2 + +z**2 - 1
 
-def plot_implicit(fn, bbox=(-2.5,2.5)):
+def plot_implicit(fn, bbox=(-2.5,2.5), ax = None):
     ''' create a plot of an implicit function
     fn  ...implicit function (plot where fn==0)
-    bbox ..the x,y,and z limits of plotted interval'''
+    bbox ..the x,y,and z limits of plotted interval
+    
+    >>> ax = plot_implicit(surface_implicit)
+    >>> ax.__class__.__name__ == 'Axes3DSubplot'
+    True
+    ''' 
+    if not ax:
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
     xmin, xmax, ymin, ymax, zmin, zmax = bbox*3
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
     A = np.linspace(xmin, xmax, 100) # resolution of the contour
     B = np.linspace(xmin, xmax, 15) # number of slices
     A1,A2 = np.meshgrid(A,A) # grid on which the contour is plotted
-
+    
     for z in B: # plot contours in the XY plane
         X,Y = A1,A2
         Z = fn(X,Y,z)
-        cset = ax.contour(X, Y, Z+z, [z], zdir='z')
+        ax.contour(X, Y, Z+z, [z], zdir='z')
         # [z] defines the only level to plot for this contour for this value of z
 
     for y in B: # plot contours in the XZ plane
         X,Z = A1,A2
         Y = fn(X,y,Z)
-        cset = ax.contour(X, Y+y, Z, [y], zdir='y')
+        ax.contour(X, Y+y, Z, [y], zdir='y')
 
     for x in B: # plot contours in the YZ plane
         Y,Z = A1,A2
         X = fn(x,Y,Z)
-        cset = ax.contour(X+x, Y, Z, [x], zdir='x')
+        ax.contour(X+x, Y, Z, [x], zdir='x')
 
     # must set plot limits because the contour will likely extend
     # way beyond the displayed level.  Otherwise matplotlib extends the plot limits
@@ -48,7 +54,9 @@ def plot_implicit(fn, bbox=(-2.5,2.5)):
     ax.set_ylim3d(ymin,ymax)
 
     plt.show()
+    return ax
     
 if __name__ == '__main__':
     
-    plot_implicit(surface_implicit)
+    import doctest
+    doctest.testmod()
